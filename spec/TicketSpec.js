65a71f9de872d0355fd5f519f333eb1738ad0e48
kkavita92 <kkavita92@gmail.com>
2017-08-05 16:46:50 +0100

    Allow each ticket to have randomly assigned price

--- spec/TicketSpec.js
+++ spec/TicketSpec.js
@@ -1,1 +1,22 @@
 'use strict';
+
+describe("Ticket", function() {
+
+  var ticket;
+
+  beforeEach(function() {
+    ticket = new Ticket();
+  });
+
+  it("has a price greater than 0", function() {
+    expect(ticket.price).toBeGreaterThan(0);
+    console.log(ticket.price);
+  });
+
+  it("has a price lesser than 100", function() {
+    expect(ticket.price).toBeLessThan(100);
+    console.log(ticket.price);
+  });
+
+
+});
--- src/Ticket.js
+++ src/Ticket.js
@@ -1,1 +1,5 @@
 'use strict';
+
+var Ticket = function() {
+  this.price = (Math.random() * 99 + 1).toFixed(2);
+};
