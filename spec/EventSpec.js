4e19014a5250fb4bd062fe1ee1d684f063d3327c
kkavita92 <kkavita92@gmail.com>
2017-08-06 10:36:30 +0100

    Implement default maximum price to remove hardcoded numbers and create #eventCapacity to generate accurate number of tickets

--- spec/EventSpec.js
+++ spec/EventSpec.js
@@ -5,18 +5,24 @@ describe("Event", function() {
   var event;
   var eventID = 1;
 
-  beforeEach(function() {
+  beforeEach( function() {
     event = new Event(eventID); // Remove hardcoded eventID
   });
 
-  it("has a numeric eventID", function () {
+  it("has a numeric eventID", function() {
     expect(event.eventID).toEqual(1)
   });
 
-  it("has more than zero tickets", function () {
-    expect(event.tickets.length).toEqual(100);
-    console.log(event.tickets[0].price);
-    console.log(event.tickets[1].price);
+  it("stores more than zero tickets", function() {
+    expect(event.tickets.length).toBeGreaterThan(0);
+  });
+
+  it("stores number of tickets equal to event capacity", function() {
+    expect(event.tickets.length).toEqual(event.eventCapacity);
+  });
+
+  it("has tickets of equivalent prices", function() {
+    expect(event.tickets[0].price).toEqual(event.tickets[1].price);
   });
 
 });
--- src/Event.js
+++ src/Event.js
@@ -1,18 +1,26 @@
 'use strict';
 
 var Event = function(eventID) {
-  this.eventID = eventID
-  this.eventPrice = this.setTicketPrice(100)
+  this.eventID = eventID;
+  this.DEFAULT_MAXIMUM_PRICE = 100;
+  this.eventPrice = this.setTicketPrice(this.DEFAULT_MAXIMUM_PRICE);
+  this.eventCapacity = this.setEventCapacity()
   this.tickets = [];
   this.generateTickets();
 };
 
-Event.prototype.generateTickets = function () {
-  for(var i = 0; i < 100; i++) {
+Event.prototype.generateTickets = function() {
+  var numberOfTickets = this.eventCapacity;
+
+  for( var i = 0; i < numberOfTickets ; i++ ) {
     this.tickets.push(new Ticket(this.eventPrice));
-  };
+  };  
 };
 
 Event.prototype.setTicketPrice = function(maximumPrice) {
   return (Math.random() * (maximumPrice - 1) + 1).toFixed(2);
+};
+
+Event.prototype.setEventCapacity = function() {
+  return Math.floor((Math.random() * 150) + 1);
 };
