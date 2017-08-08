93ce749a2426d7389561d470952dfa10ed57acad
kkavita92 <kkavita92@gmail.com>
2017-08-08 12:43:54 +0100

    Create World object with #createWorld method that instantiates new grid

---
+++ src/World.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var World = function(width, height) {
+  this.width = width;
+  this.height = height;
+  this.grid = null
+};
+
+World.prototype.createWorld = function() {
+  var newWorld = new Array();
+  for (var i = 0; i < this.height; i++ ) {
+    newWorld[i] = new Array(this.width);
+  }
+  this.grid = newWorld;
+};
