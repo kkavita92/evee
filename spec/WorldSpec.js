42a41b01fca9ff98ce56885701c95d6f2e7e2bcb
kkavita92 <kkavita92@gmail.com>
2017-08-09 18:26:33 +0100

    DRY out creating world in World test

--- spec/WorldSpec.js
+++ spec/WorldSpec.js
@@ -6,27 +6,30 @@ describe("World", function() {
 
   beforeEach(function() {
     world = new World(20, 20);
+    world.createWorld();
   });
 
   it("can create a world and assign it to grid", function() {
-    world.createWorld();
     expect(world.grid).toBeDefined();
   });
 
   it("world grid has same number of rows as height", function() {
-    world.createWorld();
     expect(world.grid.length).toEqual(world.height);
   });
 
   it("world grid has same number of columns as width", function(){
-    world.createWorld(); // making test more thorough?
     expect(world.grid[0].length).toEqual(world.width);
+    // make test more thorough?
+  });
+
+  it("populate world fills grid with events", function() {
+    world.populateWorld();
+    // expect(world.grid).toContain(2);
   });
 
   describe("Population randomiser", function () {
 
     it("returns either 1 or 0", function() {
-      world.createWorld();
       var randomiserReturnsZero = (world.populationRandomiser() == 0);
       var randomiserReturnsOne = (world.populationRandomiser() == 1);
       expect(randomiserReturnsZero || randomiserReturnsOne).toBe(true);
