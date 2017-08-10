25e1b7b617a40ed363973aaebf813d51e22134cc
kkavita92 <kkavita92@gmail.com>
2017-08-10 06:40:38 +0100

    Complete results page styling

--- jquery.js
+++ jquery.js
@@ -4,17 +4,28 @@ $(document).ready(function() {
   $('#submitcoordinates').click(function() {
     app.getUserCoordinates($("#xcoordinate").val(), $("#ycoordinate").val())
     $( '#enter-coordinates' ).hide(function() {
-    $( '#enter-coordinates' ).remove();
-    $('#user-status').text(`Your coordinates are: ${app.userCoordinates}`)
-    for (var i = 0; i < 5; i++ ) {
-      $('#content').append(`<p>${app.returnList()[i]}</p>`);
-    };
-    $('#content').append("<button>Reset</button>");
-  });
- })
+      $( '#enter-coordinates' ).remove();
+      $('#user-status').text(`Your coordinates are: ${app.userCoordinates}`)
+      $('#user-status').append("<br>Your closest events are:</br>")
+      for (var i = 0; i < 5; i++ ) {
+        $('#content').append(`<p>${app.returnList()[i]}</p>`);
+      };
+      $('#content').append("<button class='button'>Reset</button>");
+      $('#content').css({
+        "background-color" : "FloralWhite",
+        "width": "90%",
+        "border-radius": "25px",
+        "display": "block",
+        "margin-left": "auto",
+        "margin-right": "auto",
+        "padding-top": "5px",
+        "padding-bottom": "10px"    
+      });
+    });
+  })
 
-$('#content').on("click", "button", function(){
+  $('#content').on("click", "button", function(){
     location.reload()
-});
+  });
 
 });
