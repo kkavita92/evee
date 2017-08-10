6cf4532763e5b06020fa36d19e82fe14f0e61456
kkavita92 <kkavita92@gmail.com>
2017-08-10 08:36:15 +0100

    Refactor jQuery so it is not a gruesome chunk of code

--- jquery.js
+++ jquery.js
@@ -1,40 +1,42 @@
 $(document).ready(function() {
+
   var app = new Interface( 20, 20 );
 
   $('#submitcoordinates').click(function() {
     app.getUserCoordinates($("#xcoordinate").val(), $("#ycoordinate").val());
+    hideLocationForm();
+    showResults();
+  });
+
+  function hideLocationForm() {
     $( '#enter-coordinates' ).hide(function() {
       $( '#enter-coordinates' ).remove();
     });
+  }
+
+  function showResults() {
     updateUserStatus();
-    printEventSearch();
+    printEventList();
     addResetButton();
     addResultsDisplay();
-  });
-
-  $('#content').on("click", "button", function(){
-    location.reload();
-  });
+  }
 
   function updateUserStatus() {
     $('#user-status').text(`Your coordinates are: ${app.userCoordinates}`)
     $('#user-status').append("<br>Your closest events are:</br>");
   }
 
-  function printEventSearch() {
-    console.log(app.closestEventsList());
-    $('#content').append(`<p>${app.closestEventsList()[0]}</p>`);
-    $('#content').append(`<p>${app.closestEventsList()[1]}</p>`);
-    $('#content').append(`<p>${app.closestEventsList()[2]}</p>`);
-    $('#content').append(`<p>${app.closestEventsList()[3]}</p>`);
-    $('#content').append(`<p>${app.closestEventsList()[4]}</p>`);
-    // for (var i = 0; i < 5; i++ ) {
-    //   $('#content').append(`<p>${app.closestEventsList()[i]}</p>`);
-    // }
+  function printEventList() {
+    for (var i = 0; i < 5; i++ ) {
+      $('#content').append(`<p>${app.closestEventsList()[i]}</p>`);
+    }
   }
 
   function addResetButton() {
     $('#content').append("<button class='button'>Reset</button>");
+    $('#content').on("click", "button", function(){
+      location.reload();
+    });
   }
 
   function addResultsDisplay() {
